9ef397692e80c91336864d1d9568fd4adf571107
Kartikeya Bhardwaj <kartikeyabhardwaj98@gmail.com>
2018-03-29 18:23:15 +0530

    Add files via upload

---
+++ diabetespredictor.py
@@ -0,0 +1,32 @@
+import numpy as np
+from keras.models import Sequential
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+from keras.layers import *
+
+training_data_df = pd.read_csv('diabetes_data_training_scaled.csv')
+
+X = training_data_df.drop('Outcome', axis =1).values 
+Y = training_data_df[['Outcome']].values
+
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size = 0.1)
+
+#define model
+model = Sequential()
+model.add(Dense(50 , input_dim = 8, activation = 'relu'))
+model.add(Dense(200 , activation='relu'))
+model.add(Dropout(0.8))
+model.add(Dense(200, activation= 'relu'))
+model.add(Dropout(0.8))
+model.add(Dense(100, activation= 'relu'))
+model.add(Dropout(0.8))
+model.add(Dense(50, activation= 'relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss = 'mean_squared_error', optimizer = 'adam')
+
+#training the model
+model.fit(X_train,Y_train, epochs= 50 , verbose = 2 , shuffle = True)
+
+test_error_rate = model.evaluate(X_test,Y_test,verbose=0)
+Accuracy = (1-test_error_rate)*100
+print("The test error rate is:", test_error_rate,"Accuracy is",Accuracy,"%")
---
+++ preprocessing.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+#training data which is not normalised
+training_data = pd.read_csv('diabetes.csv')
+
+#using the MinMaxScaler for Normalization
+scaler = MinMaxScaler(feature_range=(0,1))
+
+#normalizing the data
+training_data_scaled = scaler.fit_transform(training_data)
+
+#saving the scaled data as dataframe object
+scaled_training_df = pd.DataFrame(training_data_scaled, columns=training_data.columns.values)
+
+#saving the data frame as a csv file
+scaled_training_df.to_csv("diabetes_data_training_scaled.csv", index=False)
